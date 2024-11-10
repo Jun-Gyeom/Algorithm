c339e7c893763db8bbe6bdd64318cb2421a6a1ab
Jun-Gyeom <109446720+Jun-Gyeom@users.noreply.github.com>
2024-11-10 14:13:10 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input;
+    cin >> input;
+
+    int n = 1;
+    while (n <= input)
+    {
+        cout << n << '\n';
+        n++;
+    }
+    return 0;
+}
