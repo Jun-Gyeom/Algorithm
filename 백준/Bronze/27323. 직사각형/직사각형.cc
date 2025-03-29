c2d4bc7e85effb25176654ab94fd3765661ee2c6
Jun-Gyeom <109446720+Jun-Gyeom@users.noreply.github.com>
2025-03-29 15:48:08 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int A = 0, B = 0;
+    cin >> A >> B;
+    cout << A * B << '\n';
+
+    return 0;
+}
+
+
