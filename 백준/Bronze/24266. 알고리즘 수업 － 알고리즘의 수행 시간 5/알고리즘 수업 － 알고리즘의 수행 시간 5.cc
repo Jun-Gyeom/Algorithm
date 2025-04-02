ddc1303071b167d4a0b922a6be35a4fd15a9ea41
Jun-Gyeom <109446720+Jun-Gyeom@users.noreply.github.com>
2025-04-02 16:32:05 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+#define ull unsigned long long
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ull n = 0;
+    cin >> n;
+    cout << n * n * n << '\n';
+    cout << "3\n";
+
+    return 0;
+}
