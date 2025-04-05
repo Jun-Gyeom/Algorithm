b63840e20861e27421bcba32d96aa1a410426e41
Jun-Gyeom <109446720+Jun-Gyeom@users.noreply.github.com>
2025-04-06 02:20:54 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 44 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define POINT pair<int, int>
+
+bool compare(const POINT& a, const POINT& b);
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N = 0;
+    cin >> N;
+
+    vector<POINT> points(N, make_pair(0, 0));
+    for (int i = 0; i < N; i++) {
+        cin >> points[i].first >> points[i].second;
+    }
+    sort(points.begin(), points.end(), compare);
+
+    for (int i = 0; i < N; i++) {
+        cout << points[i].first << ' ' << points[i].second << '\n';
+    }
+
+    return 0;
+}
+
+bool compare(const POINT& a, const POINT& b) {
+    if (a.first == b.first) {
+        return a.second < b.second;
+    }
+    return a.first < b.first;
+}
