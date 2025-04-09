88dcd3f57f8d9cc45fbb502a2ff61bbf79c662e3
Jun-Gyeom <109446720+Jun-Gyeom@users.noreply.github.com>
2025-04-09 16:13:46 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string text;
+    while (true) {
+        getline(cin, text, '.');
+        cin.ignore();
+        if (text == "") break;
+
+        bool isBalance = true;
+        stack<char> stack;
+        for (int i = 0; i < text.length(); i++) {
+            if (text[i] == '(') {
+                stack.push(text[i]);
+            }
+            else if (text[i] == '[') {
+                stack.push(text[i]);
+            }
+            else if (text[i] == ')') {
+                if (stack.empty() || stack.top() == '[') {
+                    isBalance = false;
+                    break;
+                }
+                stack.pop();
+            }
+            else if (text[i] == ']') {
+                if (stack.empty() || stack.top() == '(') {
+                    isBalance = false;
+                    break;
+                }
+                stack.pop();
+            }
+        }
+
+        if (stack.empty() && isBalance) {
+            cout << "yes\n";
+        }
+        else {
+            cout << "no\n";
+        }
+    }
+
+    return 0;
+}
