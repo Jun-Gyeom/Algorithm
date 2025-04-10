80bf47cfb84516ad0661f02b0c12a55c4c1b61ff
Jun-Gyeom <109446720+Jun-Gyeom@users.noreply.github.com>
2025-04-10 13:54:13 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N = 0;
+    cin >> N;
+
+    cout << N * (N - 1) << '\n';
+
+    return 0;
+}
