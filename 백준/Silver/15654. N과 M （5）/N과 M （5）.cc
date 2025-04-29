5bdfb547c7aea8cf8d57151f8b81daf8567ed74e
Jun-Gyeom <109446720+Jun-Gyeom@users.noreply.github.com>
2025-04-30 04:22:33 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector<int> number;
+vector<int> sequence;
+vector<bool> visited;
+
+void BackTracking(int k);
+
+int main(int argc, char* argv[]) {
+	ios::sync_with_stdio(false); 
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N >> M;
+	number.resize(N, 0);
+	sequence.resize(M, 0);
+	visited.resize(N, false);
+
+	for (int i = 0; i < N; i++) {
+		cin >> number[i];
+	}
+	sort(number.rbegin(), number.rend());
+
+	BackTracking(0);
+	
+	return 0; 
+}
+
+void BackTracking(int k) {
+	if (k == M) {
+		for (int i = 0; i < M; i++) {
+			cout << sequence[i] << ' ';
+		}
+		cout << '\n';
+
+		return;
+	}
+
+	for (int i = N - 1; i >= 0; i--) {
+		if (visited[i]) continue;
+
+		sequence[k] = number[i];
+		visited[i] = true;
+		BackTracking(k + 1);
+		visited[i] = false;
+	}
+}
